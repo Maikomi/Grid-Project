459cfadf037940b93a37f97328d1a52b255d39f3
Maikomi <maja.ochmanska@gmail.com>
2022-07-26 15:58:16 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const btn1 = document.getElementById('one');
+// btn1.onclick = () => {document.getElementById('one').style.backgroundColor = "red"}
+
+btn1.onclick = () => {
+  const randomColor = Math.floor(Math.random()*16777215).toString(16);
+  document.getElementById('one').style.backgroundColor = "#" + randomColor;
+  color.innerHTML = "#" + randomColor;
+}
+
+
+const btn2 = document.getElementById('two');
+btn2.onclick = () => alert('Hej, to łaskocze. Hi hi hi');
+
+
+const btn5 = document.getElementById('five');
+
+btn5.onclick = () => {
+    let newBg = document.getElementById('two').style.backgroundColor;
+    if(newBg === 'red') {
+      document.getElementById('two').style.backgroundColor = "blue";
+      document.getElementById('three').style.backgroundColor = "blue";
+    } else {
+      document.getElementById('two').style.backgroundColor = "red";
+      document.getElementById('three').style.backgroundColor = "red";
+    }
+}
