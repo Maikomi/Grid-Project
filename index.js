6d79f47f02cb57dff249ac3464bf403824e4be1c
Maikomi <maja.ochmanska@gmail.com>
2022-08-01 13:39:43 +0200

    refactor

--- index.js
+++ index.js
@@ -1,34 +1,42 @@
 const btn1 = document.getElementById("one");
+const btn2 = document.getElementById("two");
+const btn3 = document.getElementById("three");
+const btn4 = document.getElementById("four");
+const btn5 = document.getElementById("five");
+const btn6 = document.getElementById("six");
+const btn7 = document.getElementById("seven");
+const btn8 = document.querySelector("button");
+const btn9 = document.getElementById("nine");
+const btn10 = document.getElementById("ten");
+
 btn1.addEventListener('click', () => {
   const randomColor = Math.floor(Math.random() * 16777215).toString(16);
   document.body.style.backgroundColor = "#" + randomColor;
   color.innerHTML = "#" + randomColor;
 });
 
-const btn2 = document.getElementById("two");
+
 btn2.addEventListener('click',() => alert("Hej, to łaskocze. Hi hi hi"));
 
-let mE = document.getElementById('mE');
-const btn3 = document.getElementById("three");
-let count = 0;
-btn3.addEventListener('mouseenter',() => {
-  count++;
-});
-btn3.addEventListener('click', () => {
-  mE.textContent = "Mouseenter events count: " + count;
-  count = 0;
-});
 
-const btn4 = document.getElementById("four");
-let counter = 0;
-let mL = document.getElementById('mL');
-btn4.addEventListener('mouseleave', () => {
-  counter++;
-});
-btn4.addEventListener('click', () => {
-  mL.textContent = "Mouseleave events count: " + counter;
-  counter = 0;
-});
+const increaseCounter = (e) => {
+  const textContent = e.target.textContent.trim();
+  const splittedTextContent = textContent.split(':');
+  let counter = splittedTextContent[1];
+  const newTextContent = `${e.type} counter: ${++counter}`;
+  e.target.textContent = newTextContent;
+};
+
+const addListeners = child => {
+  const textCount = child.lastChild;
+  const textContent = textCount.textContent.trim();
+  const splittedTextContent = textContent.split(' ');
+  const event = splittedTextContent[0];
+  child.lastChild.addEventListener(event, increaseCounter);
+};
+
+const gridChildren = [...document.getElementsByClassName('counter')];
+gridChildren.forEach(addListeners);
 
 const kolor = () =>{
     let newBg = document.getElementById("two").style.backgroundColor;
@@ -41,16 +49,16 @@ const kolor = () =>{
   }
 };
 
-const btn5 = document.getElementById("five");
+
 btn5.addEventListener('click', kolor);
 
-const btn6 = document.getElementById("six");
+
 btn6.addEventListener('dblclick', () => {
   let newBg = document.getElementById("six").style;
   btn6.classList.toggle("invert");
 });
 
-const btn7 = document.getElementById("seven");
+
 btn7.addEventListener('mouseover', () => {
   btn7.classList.toggle("changeSize");
 });
@@ -58,14 +66,14 @@ btn7.addEventListener('mouseleave', () => {
   btn7.classList.toggle("changeSize");
 });
 
-const btn8 = document.querySelector("button");
+
 let text = document.querySelector("h3");
 btn8.addEventListener('click', () => {
   text.classList.toggle("visibility");
 });
 
 let modal = document.getElementById("myModal");
-const btn9 = document.getElementById("nine");
+
 
 btn9.addEventListener('click', () => {
   modal.style.display = "block";
@@ -76,15 +84,15 @@ window.addEventListener('click', (event) => {
   }
 });
 
-const btn10 = document.getElementById("ten");
+
 btn10.addEventListener('click', () => {
   document.body.classList.toggle("font");
 });
 
 const add = () => {
-  if (!document.getElementById("cntr").innerHTML.includes("you")) {
+  if (!document.getElementById("cntr").innerHTML.includes("youtube")) {
     if (window.innerWidth > 1000) {
-      const new11 = document.createElement("div");
+      const new11 = document.createElement("section");
       new11.id = "eleven";
       new11.className = "item";
       new11.innerHTML =
